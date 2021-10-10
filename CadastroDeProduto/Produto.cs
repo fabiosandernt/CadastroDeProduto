5144e12f23e1c122ab3f6d527954b33e42981fb0
fabiosandernt <44552654+fabiosandernt@users.noreply.github.com>
2021-10-10 16:45:59 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroDeProduto/Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $"
+                + Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $"
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+        public void AdicionarProdutos(int quantity)
+        {
+            Quantidade = Quantidade + quantity;
+        }
+        public void RemoverProdutos(int quantity)
+        {
+            Quantidade = Quantidade - quantity;
+        }
+    }
+}
---
+++ CadastroDeProduto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto produto;
+            produto = new Produto();
+
+            Console.WriteLine("Entre com dados do Produto: Nome, Preço e Quantidade:");
+            produto.Nome = Console.ReadLine();
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            produto.Quantidade = int.Parse(Console.ReadLine());
+            Console.WriteLine("Dados do Produto: " + produto);
+            Console.WriteLine("Digite quantidade a adicionar no estoque:");
+            //int quantidade = int.Parse(Console.ReadLine());
+
+            produto.AdicionarProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados do Produto: " + produto);
+            Console.WriteLine("Digite quantidade a remover no estoque:");
+            produto.RemoverProdutos(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Dados do Produto: " + produto);
+        }
+
+
+    }
+}
